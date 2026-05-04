fd0ff224da4d5ac010e7aa713526505c83b48453
Khalil_DeV <khalilooyoussef@gmail.com>
2026-05-04 14:12:03 +0300

    Improve app performance and resolve build/lint errors

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
-import type { NextConfig } from "next";
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
